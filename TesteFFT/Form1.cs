28480b0092b8224999f33766b1b4497b1ee1dd9c
Gabriel Cardoso <gabrielcardososantos52@hotmail.com>
2020-05-18 01:01:18 -0300

    - att graphics working

--- TesteFFT/Form1.cs
+++ TesteFFT/Form1.cs
@@ -146,12 +146,14 @@ namespace TesteFFT
     {
         private Graphics graphics;
         private List<double> values;
+        private List<Point> pontos;
         private Panel panel;
 
         public Graph(Panel panel, List<double> v)
         {
             this.panel = panel;
             graphics = panel.CreateGraphics();
+            pontos = new List<Point>();
             values = v;
         }
 
@@ -163,25 +165,42 @@ namespace TesteFFT
 
         public void Plota(int posX, int posY)
         {
-            //if (values.Count < 1)
-            //    return;
+            if (values.Count < 1)
+                return;
 
-            //var max_value = values.Max();
-            //var min_value = values.Min();
+            var max_value = values.Max();
+            var min_value = values.Min();
+
+            double escala;
+            if (Math.Abs(min_value) > Math.Abs(max_value))
+                escala = min_value;
+            else
+                escala = max_value;
 
             var panel_X = panel.Location.X;
             var panel_Y = panel.Location.Y;
 
-            
-            DrawPoint(panel_X + posX, panel_Y + posY);
+            var inc = panel.Size.Width / values.Count;
+            var y_max = panel_Y / (escala * 2);
+
+            for(int i = 0; i < values.Count; i++)
+            {
+                pontos.Add(new Point(panel_X + (i * inc), Convert.ToInt32((y_max * values[i]) + panel.Size.Height / 2)));
+                DrawPoint(pontos[i].X, pontos[i].Y);
+            }
+
+            for(int i = 0; i < values.Count - 1; i++)
+            {
+                DrawLine(pontos[i], pontos[i+1]);
+            }
         }
 
         private void DrawPoint(int x, int y)
         {
             Pen p = new Pen(Color.Black);
             SolidBrush sb = new SolidBrush(Color.Black);
-            graphics.DrawEllipse(p, x, y, 5, 5);
-            graphics.FillEllipse(sb, x, y, 5, 5);
+            graphics.DrawEllipse(p, x - 2, y - 2, 5, 5);
+            graphics.FillEllipse(sb, x - 2, y - 2, 5, 5);
         }
 
         private void DrawLine(Point p1, Point p2)
